141001d773e37b7f0fd90e233838c711637613eb
Max Khanov <maximkhanov.ku@gmail.com>
2021-06-02 02:24:13 -0500

    PYTORCH!!!

--- test/xor.py
+++ test/xor.py
@@ -9,7 +9,7 @@ model = lay_holder([lay(2, 11, True), lay(11, 11, True), lay(11, 11, True), lay(
 Xs = np.array([[0.,0.],[1.,0.],[0.,1.],[1.,1.]])
 Xs = np.insert(Xs, Xs.shape[-1], np.ones(Xs.shape[0]), axis=1).T
 
-Ys_wish = np.array([[0.],[0.],[1.],[0.]])
+Ys_wish = np.array([[0.],[1.],[1.],[0.]])
 Ys_wish = np.insert(Ys_wish, Ys_wish.shape[-1], np.ones(Ys_wish.shape[0]), axis=1).T
 print(Xs.shape)
 print(Ys_wish.shape)
--- torch_linfit.py
+++ torch_linfit.py
@@ -1,53 +1,52 @@
 from typing import List
-import numpy as np
-from funcy import print_durations
+import torch
+
+USE_CUDA = True
 
 # approximates the solution for A x = y, e.g. linear regression
-@print_durations()
-def appx_solve(x, y):
-    print(x.shape)
-    print(y.shape)
-    return (np.linalg.pinv(x.T) @ y.T).T
+def appx_solve(x, y) -> torch.Tensor:
+    return (torch.linalg.pinv(x.T) @ y.T).T
 
 class lay:
     def __init__(self, x_dim, y_dim, relu=True) -> None:
         #self.f = np.random.randn(y_dim + 1, x_dim + 1)
-        self.f = np.random.rand(y_dim + 1, x_dim + 1)# * (np.sqrt(y_dim))
+        self.f = torch.FloatTensor(y_dim + 1, x_dim + 1).uniform_() #torch.rand(y_dim + 1, x_dim + 1)# * (np.sqrt(y_dim))
+        if USE_CUDA: self.f = self.f.cuda()
         # self.f = np.abs(self.f)
 
-        self.f[-1] = ([0]*x_dim) + [1]
+        self.f[-1] = torch.cat((torch.zeros(self.f.shape[-1] - 1), torch.ones(1)), 0)
         self.do_relu = relu
     
-    @print_durations()
-    def forward(self, x): #include bias column on first layer
+    def forward(self, x) -> torch.Tensor: #include bias column on first layer
         outp = self.f @ x
-        if self.do_relu: outp = np.maximum(outp, 0)
+        if self.do_relu: outp = torch.relu(outp)
         return outp
 
-    @print_durations()
-    def update(self, new_f, correct = True):
+    def update(self, new_f, correct = True) -> None:
         self.f = new_f
-        if correct: self.f[-1] = ([0]*(self.f.shape[-1] - 1)) + [1]
+        if correct: self.f[-1] = torch.cat((torch.zeros(self.f.shape[-1] - 1), torch.ones(1)), 0)
 
 class lay_holder:
     def __init__(self, lays:List[lay]) -> None:
         if lays == None: raise ValueError("Invalid set of layers")
         self.layers = lays
 
-    @print_durations()
-    def forward(self, x, n = None):
+    def forward(self, x:torch.Tensor, n = None) -> torch.Tensor:
         if n == None: n = len(self.layers)
         elif n == -1: return x
+        x = x.cuda()
         result = x
         for layer in self.layers[:n]:
             result = layer.forward(result)
         return result
 
-    @print_durations()
-    def backward_mult(self, n = -1):
+    def backward_mult(self, n = -1) -> torch.Tensor:
         if n == -1: n = len(self.layers)
         # it is more useful if n == 0 we return the identity matrix!
-        elif n == 0: return np.eye(self.layers[-1].f.shape[0]) #create an identity matrix with the shape of the output.
+        elif n == 0:
+            #create an identity matrix with the shape of the output.
+            if USE_CUDA: return torch.eye(self.layers[-1].f.shape[0]).cuda()
+            else: return torch.eye(self.layers[-1].f.shape[0])
 
         if len(self.layers) - n < 0:
             raise ValueError("Can't backward that many layers!")
@@ -60,20 +59,14 @@ class lay_holder:
         
         return prod
     
-    @print_durations()
-    def solve_lay(self, Xs, Ys, n=-1):
+    def solve_lay(self, Xs, Ys, n=-1) -> torch.Tensor:
         if n == -1: n = len(self.layers)
-        print("doing back")
         backw = self.backward_mult(len(self.layers) - n)
-        print("doing mult")
-        backw_val = np.linalg.pinv(backw) @ Ys
-        print("doing forward")
+        backw_val = torch.linalg.pinv(backw) @ Ys
         forw_val = self.forward(Xs, n - 1)
-        print("doing solve")
         return appx_solve(forw_val, backw_val)
 
-    @print_durations()
-    def solve_update(self, Xs, Ys, n):
+    def solve_update(self, Xs, Ys, n) -> None:
         if n == 0: raise ValueError("Can't solve for 0th layer!")
         solution = self.solve_lay(Xs, Ys, n)
         self.layers[n - 1].update(solution)
